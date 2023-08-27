09edb9341f135c33fbc9a6073a1fd3dae2aa457e
gdrive9532 <133782542+gdrive9532@users.noreply.github.com>
2023-08-27 18:56:32 +0530

    Update side.tsx

--- src/components/side.tsx
+++ src/components/side.tsx
@@ -1,48 +1,39 @@
-import type {Site} from '@/types/type'
-
+import type { Site } from '@/types/type';
 import Image from 'next/image';
-import List from "@/components/list";
-
+import List from '@/components/list';
 
 interface Props {
-  sites: Site[]
-  site?: Site
-  onChange: (site: Site) => void
+  sites: Site[];
+  site?: Site;
+  onChange: (site: Site) => void;
 }
 
-
 export default function Side(props: Props) {
-
   return (
     <>
       <div className="w-0 hidden md:w-[120px] md:block">
         <div className="w-full p-1 flex flex-col h-screen">
-          <header className="flex-0"/>
-          <div
-            className="flex-1 overflow-x-hidden overflow-y-auto h-0  rounded-box bg-base-200"
-          >
+          <header className="flex-0" />
+          <div className="flex-1 overflow-x-hidden overflow-y-auto h-0 rounded-box bg-base-200">
             <div>
               <List {...props} />
             </div>
           </div>
           <footer className="h-[80px] flex items-center justify-center">
             <a className="p-1" target="_blank" href="https://github.com/zsio/Free-GPT-No-Auth">
-              <Image className="m-auto" src="/github.svg" width={26} height={26} alt="github"/>
+              <Image className="m-auto" src="/github.svg" width={26} height={26} alt="github" />
               <span className="text-xs text-center text-gray-700">star me</span>
             </a>
           </footer>
         </div>
       </div>
 
-      <div className={`${props.site?.url? 'hidden':'block'} md:hidden`}>
+      <div className={`${props.site?.url ? 'hidden' : 'block'} md:hidden`}>
         <div className="w-full p-4 mt-5 mb-5">
-          <h1 className="p-2">点击下方的网站列表，刷新页面即可返回。</h1>
+          <h1 className="p-2">Click on the website list below and refresh the page to return.</h1>
           <List {...props} />
         </div>
       </div>
-
     </>
-
-
-  )
+  );
 }
