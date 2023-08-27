ad8ea01778fd915efe280528a0c0978a04ade450
gdrive9532 <133782542+gdrive9532@users.noreply.github.com>
2023-08-27 19:00:29 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,19 @@
-import './globals.css'
-import {Inter} from 'next/font/google'
-
-import { Analytics } from '@vercel/analytics/react';
-
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: '保持更新GPT',
-  description: '收集免费、无需登录的GPT镜像站。此站点永久免费且保持更新。',
-}
-
+  title: 'Keep Updated GPT',
+  description: 'Collect free and login-free GPT mirror sites. This site is permanently free and kept up to date.',
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="zh-cn">
+    <html lang="en">
       <body className={inter.className}>{children}</body>
       <Analytics />
     </html>
-  )
+  );
 }
